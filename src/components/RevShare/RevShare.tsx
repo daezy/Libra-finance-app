7495277d9df86131b211d6c30503caff48c6ac59
Daniel Ezet <ezetdaniel@gmail.com>
2024-02-11 13:36:50 +0100

    fixed revshare

--- src/components/RevShare/RevShare.tsx
+++ src/components/RevShare/RevShare.tsx
@@ -5,16 +5,12 @@ import {
   AiOutlineClockCircle,
   AiOutlineHeatMap,
 } from "react-icons/ai";
-import Init from "./Init";
-
 const RevShare = () => {
   const ctx = useContext(AppContext);
 
   return (
     <div className="w-11/12 md:w-11/12 mx-auto mt-52 md:mt-40">
-      <div className="grid gap-5 grid-cols-1 md:grid-cols-2">
-        <Init />
-
+      <div className="grid gap-5 grid-cols-1 ">
         <div className="my-6  bg-slate-100 p-6  rounded-xl">
           <h2 className="font-bold tracking-wider">AUTO STAKE</h2>
           <div className="p-6 text-center text-slate-600">
