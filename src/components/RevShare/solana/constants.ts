5419d398c75f4248caa0dc8843a65e4c78b1c1b7
Emmanuel Oluwatobi <56241351+Tee-py@users.noreply.github.com>
2024-02-24 21:51:48 +0100

    changed rpc

--- src/components/RevShare/solana/constants.ts
+++ src/components/RevShare/solana/constants.ts
@@ -10,4 +10,4 @@ export const REV_SHARE_DATA_ACCOUNT =
 export const SOLANA_LOCAL_NET_RPC_URL = "http://127.0.0.1:8899";
 export const SOLANA_DEVNET_CONNECTION_URL = "https://api.devnet.solana.com";
 export const SOLANA_MAINNET_CONNECTION_URL =
-  "https://solana-mainnet.g.alchemy.com/v2/a0Xic8r2YTu7uJ-O-Gn27SgmDTKaelhL";
+  "https://solana-mainnet.g.alchemy.com/v2/mSSt0hYTZTKiDCQwz8TLgt7duMQLueDZ";
